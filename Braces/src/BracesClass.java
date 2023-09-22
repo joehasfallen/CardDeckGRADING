a7656886738dd86b5cfafc2a90280b8e8f08c077
Joe.Lentini24 <Joe.Lentini24@2024-313-5.bi.local>
2023-09-22 13:19:28 -0400

    code wont run, end of class

--- Braces/src/BracesClass.java
+++ Braces/src/BracesClass.java
@@ -1,4 +1,19 @@
+// Joe Lentini
+// Matching Braces 
+// Java Methods CH15 EX4
+
+import java.io.*;
+import java.util.Scanner;
 
 public class BracesClass {
+	
+	
+	public static void main(String[] args) {
+		
+		String pathName = "bText.txt";
+		
+		File bText = new File(pathName);
+	}
+	
 	
 }
